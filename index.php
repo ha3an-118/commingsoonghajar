5c19cb352b6583bfd482d7d8b0f17d6fcf1e356c
hassanabedi <habedi11100@gmail.com>
2019-03-16 11:04:53 +0330

    change to wordpress theme

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>گروه پخش لوازم یدکی پرشین</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/fonts.css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/fontawesome.css">
+  </head>
+  <body>
+      <div class="d-flex flex-column flex-nowrap justify-content-center dg-style" style="height:100%">
+
+        <div class="text-center text-light">
+          <span>
+          گروه پخش لوازم یدکی
+          </span>
+          <span class="text-warning">
+            پرشین
+          </span>
+
+        </div>
+        <div class="text-center text-secondary" style =" font-size:1.4rem">
+            سایت در حال به روز رسانی است
+        </div>
+        <div class="pt-5 text-center">
+          <div class="d-flex flex-row flex-nowrap justify-content-center">
+
+            <a href="#" class="mx-2" >
+                <i class=" text-warning fab fa-telegram"></i>
+            </a>
+
+            <a href="#" class="mx-2">
+                <i class="fab fa-instagram text-warning"></i>
+            </a>
+
+          </div>
+        </div>
+      </div>
+  </body>
+</html>
